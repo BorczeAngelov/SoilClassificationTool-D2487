76080a8cbb1621c1b0e6befcf5b94cf613779409
Borcze Angelov <Borcze.Angelov@yahoo.com>
2023-11-19 08:58:55 +0100

    small UI improvements

--- soil-classification-tool-d2487/src/app/soil-classification-result/soil-classification-result.component.ts
+++ soil-classification-tool-d2487/src/app/soil-classification-result/soil-classification-result.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-soil-classification-result',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './soil-classification-result.component.html',
   styleUrl: './soil-classification-result.component.css'
 })
